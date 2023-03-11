1bd8c30b9616af885f1592ea099c1746d13c2d37
npisor <ransomsummer@outlook.com>
2023-03-11 00:22:31 -0800

    Initial Commit

---
+++ src/main/java/com/gui/webappgui/WebAppGuiApplication.java
@@ -0,0 +1,16 @@
+package com.gui.webappgui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories("com.pmservice.basePackage.repos")
+@EntityScan("com.pmservice.basePackage.models")
+public class WebAppGuiApplication{
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppGuiApplication.class, args);
+	}
+}
