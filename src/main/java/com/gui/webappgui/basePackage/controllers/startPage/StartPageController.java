de83e489c2452f7b881499a75ca72141c039a784
npisor <ransomsummer@outlook.com>
2023-03-18 19:58:04 -0700

    Some page layout/Controller work

--- src/main/java/com/gui/webappgui/basePackage/controllers/startPage/StartPageController.java
+++ src/main/java/com/gui/webappgui/basePackage/controllers/startPage/StartPageController.java
@@ -5,14 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pmservice.basePackage.models.User.Users;
 import com.pmservice.basePackage.repos.UsersRepo;
 
@@ -26,23 +20,7 @@ public class StartPageController {
     public String index(@ModelAttribute Optional<Users> user, Model model) throws Exception{
         model.addAttribute("users", usersRepo.findById(4L).get());
         return "index";
-    }
-
-    @CrossOrigin(origins = "http://localhost:8080")
-    @RequestMapping(value = "/userSearch", method = RequestMethod.GET, produces="application/json")
-    @ResponseBody
-    public String customerSearch(String id) throws Exception
-    {
-        Optional<Users> user;
-        user = usersRepo.findById(Long.valueOf(id));
-        if(user.isEmpty()){
-            throw new Exception("No customer found with ID: " + id);
-        }
-        else{
-            ObjectMapper om = new ObjectMapper(); 
-            return om.writeValueAsString(user.get());
-        }        
-    }
+    } 
 
 }
 
--- src/main/java/com/gui/webappgui/basePackage/controllers/startPage/UserConfigurationController.java
+++ src/main/java/com/gui/webappgui/basePackage/controllers/startPage/UserConfigurationController.java
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pmservice.basePackage.models.User.Users;
 import com.pmservice.basePackage.repos.UsersRepo;
@@ -26,12 +27,13 @@ public class UserConfigurationController {
     }
 
     @CrossOrigin(origins = "http://localhost:8080")
-    @RequestMapping(value = "/createUser", method = RequestMethod.POST, produces="application/json")
+    @PostMapping("/createUser")
     @ResponseBody
     public String createNewUser(Users newUser) throws Exception
     {
+        System.out.println(newUser);
         ObjectMapper om = new ObjectMapper();
-        if(usersRepo.findById(newUser.getId()).isEmpty()){
+        if(usersRepo.findById(Long.valueOf(newUser.getId())).isEmpty()){
             usersRepo.save(newUser);
             return om.writeValueAsString("User: " + newUser.getId() + " created successfully.");
         }
